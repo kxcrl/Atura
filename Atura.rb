fe5d21a7466b5e6a5b52272b12be059b108a53cc
Luke Barbuto <luke.barbuto@gmail.com>
2012-11-10 22:45:08 -0800

    Setup initial file structure

---
+++ Atura.rb
@@ -0,0 +1,9 @@
+# Add all subfolders to the load path
+app_dir = File.expand_path(File.dirname(__FILE__))
+sub_dirs = Dir.glob(app_dir + '/**/').select { |f| File.directory? f }
+$:.push(*sub_dirs)  
+
+# Reqire all ruby files in the load path
+sub_dirs.each do |d|
+  Dir.entries(d).each { |r| require r if r =~ /\.rb$/ }
+end
