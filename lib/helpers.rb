e743f5d5cafeef843f7aa3d36efad29b8b2500fd
Luke Barbuto <luke.barbuto@gmail.com>
2012-11-20 17:20:22 -0800

    Remove extra code already in Gosu

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -34,17 +34,3 @@ module Chingu
     end
   end
 end
-
-class Numeric
-  def gosu_to_radians
-    (self - 90) * Math::PI / 180.0
-  end
-
-  def radians_to_gosu
-    self * 180.0 / Math::PI + 90
-  end
-
-  def radians_to_vec2
-    CP::Vec2.new(Math::cos(self), Math::sin(self))
-  end
-end
